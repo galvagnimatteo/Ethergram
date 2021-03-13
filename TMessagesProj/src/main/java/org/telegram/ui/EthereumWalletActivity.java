f197f98290e0b0f581d55977fb1f47793711b809
galvagnimatteo <galvagni.matteo@protonmail.com>
2021-03-13 19:53:59 +0100

    added cool animation when syncing node

--- TMessagesProj/src/main/java/org/telegram/ui/EthereumWalletActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/EthereumWalletActivity.java
@@ -1,13 +1,18 @@
 package org.telegram.ui;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.os.AsyncTask;
 import android.os.Build;
+import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.Display;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.Animation;
@@ -21,6 +26,8 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
+
 import org.ethereum.geth.Geth;
 import org.ethereum.geth.Node;
 import org.ethereum.geth.NodeConfig;
@@ -32,7 +39,6 @@ import org.telegram.ui.ActionBar.ActionBar;
 import org.telegram.ui.ActionBar.BaseFragment;
 import org.telegram.ui.ActionBar.Theme;
 import org.telegram.ui.Components.LayoutHelper;
-import org.telegram.ui.Components.Point;
 
 import java.io.File;
 
@@ -44,9 +50,10 @@ public class EthereumWalletActivity extends BaseFragment {
 
     protected View actionBarBackground;
 
-    private ImageView imageView;
+    private CircleImageView ethlogoimage;
     private TextView titleTextView;
     private TextView messageTextView;
+    private LinearLayout ethwalletheader;
 
     File dir;
 
@@ -97,14 +104,10 @@ public class EthereumWalletActivity extends BaseFragment {
                 super.onLayout(changed, left, top, right, bottom);
             }
         };
+
         fragmentView.setBackgroundColor(Theme.getColor(Theme.key_windowBackgroundGray));
         fragmentView.setTag(Theme.key_windowBackgroundGray);
         FrameLayout frameLayout = (FrameLayout) fragmentView;
-
-        LinearLayout logolayout = new LinearLayout(context);
-
-        logolayout.setLayoutParams(new LinearLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.WRAP_CONTENT));
-        logolayout.setOrientation(LinearLayout.VERTICAL);
 
         actionBarBackground = new View(context) {
 
@@ -118,18 +121,19 @@ public class EthereumWalletActivity extends BaseFragment {
                 parentLayout.drawHeaderShadow(canvas, h);
             }
         };
+
         actionBarBackground.setAlpha(0.0f);
         frameLayout.addView(actionBarBackground, LayoutHelper.createFrame(LayoutHelper.MATCH_PARENT, LayoutHelper.WRAP_CONTENT));
         frameLayout.addView(actionBar, LayoutHelper.createFrame(LayoutHelper.MATCH_PARENT, LayoutHelper.WRAP_CONTENT));
 
-        int top = (int) ((ActionBar.getCurrentActionBarHeight() + (actionBar.getOccupyStatusBar() ? AndroidUtilities.statusBarHeight : 0)) / AndroidUtilities.density) - 44;
+        View ethwalletheaderView = LayoutInflater.from(context).inflate(R.layout.ethwalletheader, null);
 
-        imageView = new CircleImageView(context);
-        imageView.setImageResource(R.drawable.ethtoken);
+        ethwalletheader = (LinearLayout) ethwalletheaderView.findViewById(R.id.ethwalletheader);
 
-        //FIXME this imageview should be on center horizontally, then add animation https://stackoverflow.com/questions/29911046/android-zoom-animation-using-animatorset
+        ethlogoimage = (CircleImageView) ethwalletheaderView.findViewById(R.id.ethlogo);
+        ethlogoimage.setImageResource(R.drawable.ethtoken);
 
-        imageView.setOnClickListener(new View.OnClickListener() {
+        ethlogoimage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -144,22 +148,20 @@ public class EthereumWalletActivity extends BaseFragment {
             }
         });
 
-        logolayout.addView(imageView, LayoutHelper.createFrame(140, 140, Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, top + 27, 0, 0));
-
-        titleTextView = new TextView(context);
+        titleTextView = (TextView) ethwalletheaderView.findViewById(R.id.titleTextView);
         titleTextView.setTextColor(Theme.getColor(Theme.key_windowBackgroundWhiteBlackText));
         titleTextView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 24);
         titleTextView.setGravity(Gravity.CENTER);
         titleTextView.setText("Your Ethereum wallet");
-        logolayout.addView(titleTextView);
 
-        messageTextView = new TextView(context);
+        messageTextView = (TextView) ethwalletheaderView.findViewById(R.id.messageTextView);
         messageTextView.setTextColor(Theme.getColor(Theme.key_windowBackgroundWhiteGrayText));
         messageTextView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 15);
         messageTextView.setGravity(Gravity.CENTER);
 
         if(GethNodeHolder.getInstance().getNode() != null){
 
+            scaleDownAnimation();
             messageTextView.setText("Node running on http://localhost:" + GethNodeHolder.getInstance().getNode().getNodeInfo().getListenerPort());
 
         }else{
@@ -168,12 +170,35 @@ public class EthereumWalletActivity extends BaseFragment {
 
         }
 
-        logolayout.addView(messageTextView);
-        frameLayout.addView(logolayout);
+        frameLayout.addView(ethwalletheader);
 
         return fragmentView;
+
     }
 
+    private void scaleDownAnimation(){
+
+        ObjectAnimator scaleDownX = ObjectAnimator.ofFloat(ethlogoimage, "scaleX", 0.5f);
+        ObjectAnimator scaleDownY = ObjectAnimator.ofFloat(ethlogoimage, "scaleY", 0.5f);
+        scaleDownX.setDuration(1000);
+        scaleDownY.setDuration(1000);
+
+        ObjectAnimator moveUpY = ObjectAnimator.ofFloat(ethlogoimage, "translationY", -155);
+        moveUpY.setDuration(1000);
+
+        ObjectAnimator moveuptext = ObjectAnimator.ofFloat(titleTextView, "translationY", -305);
+        moveuptext.setDuration(1000);
+        ObjectAnimator moveuptext2 = ObjectAnimator.ofFloat(messageTextView, "translationY", -305);
+        moveuptext2.setDuration(1000);
+
+        AnimatorSet scaleDown = new AnimatorSet();
+        AnimatorSet moveUp = new AnimatorSet();
+
+        scaleDown.play(scaleDownX).with(scaleDownY).with(moveUpY).with(moveuptext).with(moveuptext2);
+
+        scaleDown.start();
+
+    }
 
     private class CreateNode extends AsyncTask{
 
@@ -187,6 +212,18 @@ public class EthereumWalletActivity extends BaseFragment {
 
                 GethNodeHolder gethNode = GethNodeHolder.getInstance();
                 gethNode.setNode(node);
+
+                //FIXME add animation https://stackoverflow.com/questions/29911046/android-zoom-animation-using-animatorset
+
+                getParentActivity().runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+
+                        scaleDownAnimation();
+
+                    }
+                });
 
                 messageTextView.setText("Node running on http://localhost:" + gethNode.getNode().getNodeInfo().getListenerPort());
 
